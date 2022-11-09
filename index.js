ffafa62c576f2621a3fc5df4cb0de48bc587dbf1
arifusinbad <ariful.islam.sinbad@gmail.com>
2022-11-09 23:16:13 +0600

    commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
@@ -29,6 +29,12 @@ app.get('/services', async(req, res)=>{
   const result = await cursor.limit(size).toArray()
   const count =await travelCollection.estimatedDocumentCount();
   res.send({count, result})
+})
+app.get('/services/:id', async(req, res)=>{
+  const id =req.params.id
+  const query = {_id: ObjectId(id)}
+  const cursor = await travelCollection.findOne(query)
+  res.send(cursor)
 })
  }
  finally{
