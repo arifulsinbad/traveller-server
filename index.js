1c10bb718a86dc88bda5653f1e711c4053aab9e8
arifusinbad <ariful.islam.sinbad@gmail.com>
2022-11-09 14:54:51 +0600

    commit 4

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
 app.get('/', (req, res)=>{
  res.send('traveller server runing')
 })
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.y2sfurg.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+ try{
+
+ }
+ finally{
+
+ }
+}
+run().catch(error=>console.log(error))
+
+
+
+
+
 app.listen(port, (req, res)=>{
 console.log(`traveller server port ${port}`)
 })
